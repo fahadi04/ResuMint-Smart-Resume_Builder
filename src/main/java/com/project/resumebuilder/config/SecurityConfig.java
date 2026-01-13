01eec26fea7fb9b72054e34aad7a23967665e944
fahadi04 <fahadmasrur786@gmail.com>
2026-01-13 23:36:43 +0530

    Update Security Config

--- src/main/java/com/project/resumebuilder/config/SecurityConfig.java
+++ src/main/java/com/project/resumebuilder/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:*"));
+        configuration.setAllowedOrigins(Arrays.asList("https://resumint-smart-resumebuilder-production.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "" +
                 "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
